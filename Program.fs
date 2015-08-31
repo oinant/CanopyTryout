b34daf3a819cba205fe0afef1be49f01a702600a
oinant <antoine.sauvinet+github@gmail.com>
2015-08-31 11:36:04 +0200

    A tryout for canopy, f# wrapper of webdriver

---
+++ Program.fs
@@ -0,0 +1,16 @@
+﻿open canopy
+open runner
+open PageObjects.Google
+
+start firefox
+
+"first result for canopy f# in google is the project homepage"  &&& fun _ -> 
+    PageObjects.Google.goToFirstResultFor "canopy f#"
+    on "http://lefthandedgoat.github.io/canopy/"
+
+run()
+printfn "press [enter] to exit"
+System.Console.ReadLine |> ignore
+
+quit()
+
---
+++ google.fs
@@ -0,0 +1,10 @@
+﻿namespace PageObjects
+module Google =    
+    open canopy
+
+    let goToFirstResultFor (search:string) = 
+        url "https://www.google.com"
+        "#lst-ib" << search
+        click "button[name=btnG]" 
+        click (first "div.srg a")
+
